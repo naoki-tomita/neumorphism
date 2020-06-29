c3245fe7a727b48701ad3cb18072844d8c04a15c
kojiro.ueda <mxcpy608@hotmail.com>
2020-06-29 09:35:50 +0900

    Build

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,22 @@
 module.exports = {
-  stories: ['../stories/**/*.stories.[tj]sx'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+  stories: ["../stories/**/*.stories.[tj]sx"],
+  addons: ["@storybook/addon-actions", "@storybook/addon-links"],
   webpackFinal: async config => {
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
-      loader: ["ts-loader", 'react-docgen-typescript-loader']
+      use: [
+        {
+          loader: 'ts-loader',
+          options: {
+            configFile: "tsconfig.storybook.json"
+          }
+        },
+        {
+          loader: "react-docgen-typescript-loader"
+        }
+      ],
     });
-    config.resolve.extensions.push('.ts', '.tsx');
+    config.resolve.extensions.push(".ts", ".tsx");
     return config;
   }
 }
